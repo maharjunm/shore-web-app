3621ce67ffe0ff5313a4681972df10f882237bcb
CHARY-VADLA <vadlanagarajuchary2002@gmail.com>
2023-01-12 20:04:57 +0530

    removed unwanted spaces

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,31 +3,26 @@ import './NavBar.scss';
 import { Link, NavLink } from 'react-router-dom';
 const NavBar = () => {
     return (
-        <>
-            <nav className="navbar">
-                <div className="container">
-                    <div className="header">
-                    <h4>
-                        <span className="color-blue">Shore</span>
-                        <span className="color-yellow">Birdie</span>
-                    </h4>
-
-                        <span className="companyLogo"> <img  src="largelogo.png" width="100px" /></span>
-
-                    </div>
-
-                    <div>
-                      <ul>
-                          <li ><NavLink exact to="/" >Home<i className="fa-solid fa-house"></i></NavLink></li>
-                          <li > <NavLink to="/jobs">Jobs</NavLink></li>
-                          <li ><NavLink to="/postjobs">Post a Job</NavLink></li>
-                          <li > <NavLink to="/aboutus">About Us</NavLink></li>
-                          <li > <NavLink to="/contactus">Contact Us</NavLink></li>
-                      </ul>
-                    </div>
-                </div>
-            </nav>
-        </>
+        <nav className="navbar">
+          <div className="container">
+            <div className="header">
+              <h4>
+                <span className="color-blue">Shore</span>
+                <span className="color-yellow">Birdie</span>
+              </h4>
+                <span className="companyLogo"> <img  src="largelogo.png" width="100px" /></span>
+            </div>
+          <div>
+            <ul>
+              <li ><NavLink exact to="/" >Home<i className="fa-solid fa-house"></i></NavLink></li>
+              <li > <NavLink to="/jobs">Jobs</NavLink></li>
+              <li ><NavLink to="/postjobs">Post a Job</NavLink></li>
+              <li > <NavLink to="/aboutus">About Us</NavLink></li>
+              <li > <NavLink to="/contactus">Contact Us</NavLink></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
 };
 export default NavBar;
--- src/pages/ContactUs/ContactUs.tsx
+++ src/pages/ContactUs/ContactUs.tsx
@@ -11,29 +11,27 @@ const ContactUs = ( ) => {
     }
     return (
       <ErrorBoundary>
-      <div className="contactContainer">
-        <h1>Contact Us</h1>
-        <div className="infobox">
-        US ,Canada & South America 9:00 AM - 5:30 PM ,EST Phone :202-312-6375 E-mail:service@shorebirdie.org<br/>
-        International 9:00 AM - 5:30 PM GMT Phone :+44(0) 1223 326 528 E-mail:service@shorebirdie.org
-        </div>
-        <div className="contentBox">
-          <div className="writeup">
-            Our team speaks English, German, French, Spanish, Italian, Swedish, Arabic and Chineese and will respond to your query  within 24 hours.
+        <div className="contactContainer">
+          <h1>Contact Us</h1>
+          <div className="infobox">
+            US ,Canada & South America 9:00 AM - 5:30 PM ,EST Phone :202-312-6375 E-mail:service@shorebirdie.org<br/>
+            International 9:00 AM - 5:30 PM GMT Phone :+44(0) 1223 326 528 E-mail:service@shorebirdie.org
           </div>
-          <div>
-            <form onSubmit={onSubmit} action='#' >
-              <input type="email" name="email"  placeholder="Email"/>
-              <input type="text" name="name" placeholder="Full Name"/>
-              <input type="text" name="companyname" placeholder="Company Name"/>
-              <input type="text" name="query" placeholder="Query"/><br/>
-              <button type="submit" className={formStatus}> {formStatus}</button>
-
-            </form>
+          <div className="contentBox">
+            <div className="writeup">
+              Our team speaks English, German, French, Spanish, Italian, Swedish, Arabic and Chineese and will respond to your query  within 24 hours.
+            </div>
+            <div>
+              <form onSubmit={onSubmit} action='#' >
+                <input type="email" name="email"  placeholder="Email"/>
+                <input type="text" name="name" placeholder="Full Name"/>
+                <input type="text" name="companyname" placeholder="Company Name"/>
+                <input type="text" name="query" placeholder="Query"/><br/>
+                <button type="submit" className={formStatus}> {formStatus}</button>
+              </form>
+            </div>
           </div>
-
         </div>
-      </div>
       </ErrorBoundary>
     );
 };
