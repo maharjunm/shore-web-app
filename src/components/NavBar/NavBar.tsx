94490a4841b6849737a03518c728ddeb7e884219
CHARY-VADLA <vadlanagarajuchary2002@gmail.com>
2023-01-18 00:07:11 +0530

    indented in navbar and contact pages

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -14,55 +14,55 @@ const NavBar = () => {
     };
     return (
         <nav className="navbar">
-            <div className="main">
-                <div className="container">
-                    <div className="header">
-                        <Flex>
-                            <img className="img" src={Logo} />
-                            <h4>
-                                <NavLink exact to="/">
-                                    <span className="color-blue">Shore</span>
-                                    <span className="color-yellow">Birdie</span>
-                                </NavLink>
-                            </h4>
-                        </Flex>
-                    </div>
-                    <div>
-                        <ul>
-                            <li><NavLink exact to="/">Home</NavLink> </li>
-                            <li><NavLink to="/jobs">Jobs</NavLink></li>
-                            <li><NavLink to="/postjobs">Post a Job</NavLink></li>
-                            <li><NavLink to="/aboutus">About Us</NavLink></li>
-                            <li><NavLink to="/contactus">Contact Us</NavLink></li>
-                        </ul>
-                    </div>
-                </div>
+          <div className="main">
+            <div className="container">
+              <div className="header">
+                <Flex>
+                  <img className="img" src={Logo} />
+                  <h4>
+                    <NavLink exact to="/">
+                      <span className="color-blue">Shore</span>
+                      <span className="color-yellow">Birdie</span>
+                    </NavLink>
+                  </h4>
+                </Flex>
+              </div>
+              <div>
+                <ul>
+                  <li><NavLink exact to="/">Home</NavLink> </li>
+                  <li><NavLink to="/jobs">Jobs</NavLink></li>
+                  <li><NavLink to="/postjobs">Post a Job</NavLink></li>
+                  <li><NavLink to="/aboutus">About Us</NavLink></li>
+                  <li><NavLink to="/contactus">Contact Us</NavLink></li>
+                </ul>
+              </div>
             </div>
-            <div className="mobview">
-                <div className="container">
-                    <div className="header">
-                        <img className="img" src={Logo} />
-                        <h4 className="margin">
-                            <span className="color-blue">Shore</span>
-                            <span className="color-yellow">Birdie</span>
-                        </h4>
-                        <button className="menubar" onClick={onClick}>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </button>
-                    </div>
-                    <div className={'options ' + mobview}>
-                        <ul onClick={() => setMobview('invisible')}>
-                            <li><NavLink exact to="/">Home</NavLink></li>
-                            <li><NavLink to="/jobs">Jobs</NavLink></li>
-                            <li><NavLink to="/postjobs">Post a Job</NavLink></li>
-                            <li><NavLink to="/aboutus">About Us</NavLink></li>
-                            <li><NavLink to="/contactus">Contact Us</NavLink></li>
-                        </ul>
-                    </div>
-                </div>
+          </div>
+          <div className="mobview">
+            <div className="container">
+              <div className="header">
+                <img className="img" src={Logo} />
+                <h4 className="margin">
+                  <span className="color-blue">Shore</span>
+                  <span className="color-yellow">Birdie</span>
+                </h4>
+                <button className="menubar" onClick={onClick}>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </button>
+              </div>
+              <div className={'options ' + mobview}>
+                <ul onClick={() => setMobview('invisible')}>
+                  <li><NavLink exact to="/">Home</NavLink></li>
+                  <li><NavLink to="/jobs">Jobs</NavLink></li>
+                  <li><NavLink to="/postjobs">Post a Job</NavLink></li>
+                  <li><NavLink to="/aboutus">About Us</NavLink></li>
+                  <li><NavLink to="/contactus">Contact Us</NavLink></li>
+                </ul>
+              </div>
             </div>
+          </div>
         </nav>
     );
 };
