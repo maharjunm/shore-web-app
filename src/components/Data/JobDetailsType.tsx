a52a5b5f6065310972d02d7b2aeca517f9da2aad
CHARY-VADLA <vadlanagarajuchary2002@gmail.com>
2023-01-22 11:19:14 +0530

    created data structure for post a job

--- src/components/Data/JobDetailsType.tsx
+++ src/components/Data/JobDetailsType.tsx
@@ -1,3 +1,52 @@
+class Company{
+  name:string;
+  type:string;
+  logo:File;
+  Company(name:string,type:string,logo:File){
+    this.name=name;
+    this.type=type;
+    this.logo=logo;
+  }
+}
+class Location{
+  city:string;
+  state:string;
+  country:string;
+  region:string;
+  Location(city:string,state:string,country:string,region:string){
+    this.city=city;
+    this.state=state;
+    this.country=country;
+    this.region=region;
+  }
+}
+class Dates{
+  postingDate:Date;
+  expiryDate:Date;
+  closingDate:Date;
+  removingJobDate:Date;
+  Dates(postingDate:Date,expiryDate:Date,closingDate:Date,removingJobDate:Date){
+    this.postingDate=postingDate;
+    this.expiryDate=expiryDate;
+    this.closingDate=closingDate;
+    this.removingJobDate=removingJobDate;
+  }
+}
+class Salary{
+  salary:string;
+  hours:number;
+  type:string;
+  Salary(salary:string,hours:number,type:string){
+    this.salary=salary;
+    this.hours=hours;
+    this.type=type;
+  }
+}
+class Job{
+  title:  string;
+  qualification:string;
+  experience:number;  
+}
 class JobDetailsType{
   role:string;
   companyName: string;
