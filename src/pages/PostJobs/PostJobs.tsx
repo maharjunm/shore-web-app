5ad9ecff56277e7c6bd92fd34404f46987dbdbb7
CHARY-VADLA <vadlanagarajuchary2002@gmail.com>
2023-01-25 14:51:11 +0530

    created billing page

--- src/pages/PostJobs/PostJobs.tsx
+++ src/pages/PostJobs/PostJobs.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { ErrorBoundary } from '../../components';
 import ProductSelectionPage from '../ProductSelectionPage/ProductSelectionPage';
 import Form from './Form';
+import BillingPage from './BillingPage';
 const PostJobs = () => {
     return (
         <ErrorBoundary>
-            <Form />
+            <BillingPage billingType="premium" />
         </ErrorBoundary>
     );
 };
