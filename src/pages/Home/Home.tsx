1b5faa9c3b6b35d95e6d6b6f6d2fc606be64300d
amrathmd <amrathmd.mohammed@gmail.com>
2023-01-26 11:41:23 +0530

    updated reviews

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { ErrorBoundary,Searchbar, Location } from '../../components';
 import data from '../../components/SearchBar/data';
-
+import './home.scss'
 const Home = () => {
     return (
         <ErrorBoundary>
+            <div className='inputForm'>
+            <div className="searchBar">
             <Searchbar data={data} />
+            </div>
+            <div className='locationBar'>
             <Location />
+            </div>
+            </div>
         </ErrorBoundary>
     );
 };
