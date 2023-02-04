83ca1c31e283d0c8e3113fcc3d0284da9df3502c
suchitraguduru <gudurusuchi2003@gmail.com>
2023-02-03 18:51:14 -0800

    Added Payment using Stripe

--- src/components/Payment/Payment.tsx
+++ src/components/Payment/Payment.tsx
@@ -9,19 +9,19 @@ const Payment = ()=>{
     price: 200,
     description: 'This is a Sample'
   });
-  // async function handleToken(token,adresses){
-  //   const response = await axios.post('http://localhost:5000/checkout',{token,product})
-  //   console.log(response);
-  // }
+  async function handleToken(token:any){
+    const response = await axios.post('http://localhost:3000/v1/users/checkout',{token,product});
+    console.log(response);
+    return response;
+  }
+  handleToken({});
   return (
     <div className="container">
       
       <br></br>
       <StripeCheckout
         stripeKey = "pk_test_51MUr0rSCXoMBK86oohQxGrv7r21gkhp9DgomNGs6Lbgfhnhqxa8XnN4JwYHswFq5KvxjDUpgYLj5Belu5O7JWima00WiAAgjND"
-        token = {function has(){
-          return true;
-        }}
+        token = {handleToken}
         amount = {product.price*100}
         name = {product.name}
         billingAddress
