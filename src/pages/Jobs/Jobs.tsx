0c2004e08cbab8a6eaa5c18b48f5d1e8ae806487
suchitraguduru <gudurusuchi2003@gmail.com>
2023-02-10 18:51:34 -0800

    some changes

--- src/pages/Jobs/Jobs.tsx
+++ src/pages/Jobs/Jobs.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { ErrorBoundary } from '../../components';
-
+import Payment from '../../components/Payment/Payment';
 const Jobs = () => {
   return (
     <ErrorBoundary>
       <h1>jobs</h1>
+      <Payment/>
     </ErrorBoundary>
   );
 };
