fc591cf2d8c53d736fd19d538d2be112825b206d
suchitraguduru <gudurusuchi2003@gmail.com>
2023-02-10 17:47:59 -0800

    created payment with stripe

--- src/components/Payment/Payment.tsx
+++ src/components/Payment/Payment.tsx
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import axios from 'axios';
 import './Payment.scss';
+// import {ToastContainer,toast} from 'react-toastify'
+// import "react-toastify/dist/ReactToastify.css";
 const Payment = ()=>{
   //product object name,price
+  // toast.configure()
   const [product]  = useState({
     name: 'Sample Book',
     price: 200,
@@ -11,22 +14,34 @@ const Payment = ()=>{
   });
   async function handleToken(token:any){
     const response = await axios.post('http://localhost:3000/v1/users/checkout',{token,product});
-    console.log(response);
-    return response;
+    console.log(response.status);
+    if(response.status===200){
+      console.log('Success, Payment is complete',{type:'success'});
+    }else{
+      console.log('Failure, Payment is complete',{type:'error'});
+    }
+    
   }
-  handleToken({});
   return (
-    <div className="container">
+    <div className="paymentCss">
+      <div className="flexContent">
+        <h2>Product   Info</h2>
+        <h3>Product Name: {product.name}</h3>
+        <h3>Product Price: {product.price}</h3>
+        <h3>Product Description: {product.description}</h3>
+      </div>
+        
+      <div className='stripe'>
+        <StripeCheckout
+          stripeKey = "pk_test_51MUr0rSCXoMBK86oohQxGrv7r21gkhp9DgomNGs6Lbgfhnhqxa8XnN4JwYHswFq5KvxjDUpgYLj5Belu5O7JWima00WiAAgjND"
+          token = {handleToken}
+          amount = {product.price*100}
+          name = {product.name}
+          billingAddress
+          shippingAddress
+        />
+      </div>
       
-      <br></br>
-      <StripeCheckout
-        stripeKey = "pk_test_51MUr0rSCXoMBK86oohQxGrv7r21gkhp9DgomNGs6Lbgfhnhqxa8XnN4JwYHswFq5KvxjDUpgYLj5Belu5O7JWima00WiAAgjND"
-        token = {handleToken}
-        amount = {product.price*100}
-        name = {product.name}
-        billingAddress
-        shippingAddress
-      />
     </div>
   );
 };
