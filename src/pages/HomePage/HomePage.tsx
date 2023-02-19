e736271e79f53251c99a2e329c940d0067828a78
CHARY-VADLA <vadlanagarajuchary2002@gmail.com>
2023-02-20 02:00:56 +0530

    changed homepage imports

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,7 +1,7 @@
 import React,{useState} from 'react';
 import { ErrorBoundary,NavBar } from '../../components';
 import {HashRouter as Router,Switch,Route,Link} from 'react-router-dom';
-import {Home,PostJobs,ContactUs,ProductSelectionPage,BillingPage} from '../../pages';
+import {Home,TestPayment,PostJobs,ContactUs,ProductSelectionPage,BillingPage} from '../../pages';
 
 const HomePage = () => {
   return (
