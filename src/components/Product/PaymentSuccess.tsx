6636491673a7cda9fba849eee724c334b943f8dd
CHARY-VADLA <vadlanagarajuchary2002@gmail.com>
2023-04-02 18:04:45 +0530

    added message received from backend

--- src/components/Product/PaymentSuccess.tsx
+++ src/components/Product/PaymentSuccess.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import { useHistory } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import './PaymentStatus.scss';
 
 const PaymentSuccess = () => {
   const history = useHistory();
+  const location = useLocation();
+  const search = new URLSearchParams(location.search);
+  const message = search.get('message');
   const redirect = () =>{
     history.push('/profile');
     return;
@@ -15,8 +19,12 @@ const PaymentSuccess = () => {
   },5000);
   return (
     <div className='paymentStatus'>
-      <span className='icon success'> <FA icon={faCheck} /> </span>
-      <h1>Payment Successfull</h1>
+      { message &&
+        <span className='icon success'>
+          <FA icon={faCheck} />
+        </span>
+      }
+      <h1>{ message }</h1>
       <p>You will be redirected to home page shortly...</p>
     </div>
   );
