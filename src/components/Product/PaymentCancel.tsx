0da143b5d345f2f09fe34f2cc9cce3db7131f242
CHARY-VADLA <vadlanagarajuchary2002@gmail.com>
2023-04-02 18:04:45 +0530

    added message received from backend

--- src/components/Product/PaymentCancel.tsx
+++ src/components/Product/PaymentCancel.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { FontAwesomeIcon as FA } from '@fortawesome/react-fontawesome';
 import { faXmark  } from '@fortawesome/free-solid-svg-icons';
 import { useHistory } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import './PaymentStatus.scss';
 
 const PaymentCancel = () => {
   const history = useHistory();
+  const location = useLocation();
+  const search = new URLSearchParams(location.search);
+  const message = search.get('message');
   const redirect = () =>{
     history.push('/profile');
     return;
@@ -15,8 +19,12 @@ const PaymentCancel = () => {
   },5000);
   return (
     <div className='paymentStatus'>
-      <span className='icon cancel'> <FA icon={faXmark} /> </span>
-      <h1>Payment Failed</h1>
+      { message && 
+        <span className='icon cancel'>
+          <FA icon={faXmark} />
+        </span>
+      }
+      <h1>{ message }</h1>
       <p>You will be redirected to home page shortly...</p>
     </div>
   );
